c5bdd40ef0b554fb0428a4f5f70318433ae2d1d7
Amila <amila.abeygunasekara@zebra.com>
2019-08-05 14:38:58 +0530

    Implement a sample to test the opencv library

--- BarcodeTracker/app/src/main/cpp/native-lib.cpp
+++ BarcodeTracker/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,24 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/core.hpp>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_amila_barcodetracker_MainActivity_stringFromJNI(
+extern "C"
+{
+
+jstring Java_com_amila_barcodetracker_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+
+}
+
+jstring Java_com_amila_barcodetracker_MainActivity_validate(
+        JNIEnv *env, jobject, jlong addrGray, jlong addrRgba) {
+    cv::Rect();
+    cv::Mat();
+    std::string hello2 = "Hello from validate";
+    return env->NewStringUTF(hello2.c_str());
+}
+
 }
--- BarcodeTracker/app/src/main/java/com/amila/barcodetracker/MainActivity.java
+++ BarcodeTracker/app/src/main/java/com/amila/barcodetracker/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.opencv.android.OpenCVLoader;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3");
     }
 
     @Override
@@ -20,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        if (!OpenCVLoader.initDebug()) {
+            tv.setText(tv.getText() + "\nOpenCVLoader.initDebug(), Not working ");
+        } else {
+            tv.setText(tv.getText() + "\nOpenCVLoader.initDebug(), Working ");
+            tv.setText(tv.getText() + "\n" + validate(0L, 0L));
+        }
     }
 
     /**
@@ -27,4 +37,5 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native String validate(long matAddrGr, long matAddrRgba);
 }

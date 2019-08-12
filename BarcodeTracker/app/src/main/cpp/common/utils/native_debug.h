5dbdf6eb82686ea01556566858d41508d504e126
Amila Abeygunasekara <auabey@gmail.com>
2019-08-12 16:35:59 +0530

    Rename native debug log tag

--- BarcodeTracker/app/src/main/cpp/common/utils/native_debug.h
+++ BarcodeTracker/app/src/main/cpp/common/utils/native_debug.h
@@ -1,6 +1,6 @@
 #include <android/log.h>
 
-#define LOG_TAG "CAMERA-SAMPLE"
+#define LOG_TAG "BarcodeTracker"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)

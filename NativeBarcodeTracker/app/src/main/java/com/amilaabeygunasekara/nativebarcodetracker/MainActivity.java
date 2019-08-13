223899041f9a7e67a913ffd2d1aa340f16543138
Amila Abeygunasekara <auabey@gmail.com>
2019-08-13 16:09:05 +0530

    Implement SurfaceView on java layer

--- NativeBarcodeTracker/app/src/main/java/com/amilaabeygunasekara/nativebarcodetracker/MainActivity.java
+++ NativeBarcodeTracker/app/src/main/java/com/amilaabeygunasekara/nativebarcodetracker/MainActivity.java
@@ -1,16 +1,23 @@
 package com.amilaabeygunasekara.nativebarcodetracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.WindowManager;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     static {
         System.loadLibrary("native-lib");
     }
+
+    public static final String TAG = "NativeBarcodeTracker";
+
+    SurfaceView mSurfaceView;
+    SurfaceHolder mSurfaceHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +25,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        // Example of a call to a native method
-        //TextView tv = findViewById(R.id.sample_text);
-        //tv.setText(stringFromJNI());
+        mSurfaceView = (SurfaceView)findViewById(R.id.texturePreview);
+        mSurfaceHolder = mSurfaceView.getHolder();
+
+        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                Canvas canvas = mSurfaceHolder.lockCanvas();
+                if (canvas != null) {
+                    canvas.drawColor(Color.BLUE);
+                    mSurfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+            }
+        });
     }
 
     public native String stringFromJNI();

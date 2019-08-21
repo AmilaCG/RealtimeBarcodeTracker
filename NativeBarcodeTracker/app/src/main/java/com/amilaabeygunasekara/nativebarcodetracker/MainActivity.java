00157cb86909a469dc010edcee2d02a0310f24bd
Amila Abeygunasekara <auabey@gmail.com>
2019-08-21 11:54:01 +0530

    Fix camera preview not displaying after granting permission for the first time

--- NativeBarcodeTracker/app/src/main/java/com/amilaabeygunasekara/nativebarcodetracker/MainActivity.java
+++ NativeBarcodeTracker/app/src/main/java/com/amilaabeygunasekara/nativebarcodetracker/MainActivity.java
@@ -2,10 +2,7 @@ package com.amilaabeygunasekara.nativebarcodetracker;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Surface;
@@ -18,8 +15,6 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends Activity {
-
-    private static Context mContext;
 
     static {
         System.loadLibrary("native-tracker");
@@ -37,32 +32,31 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        mContext = getApplicationContext();
-
         mSurfaceView = (SurfaceView)findViewById(R.id.texturePreview);
         mSurfaceHolder = mSurfaceView.getHolder();
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestCameraPermission();
-        } else {
-            mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
-                @Override
-                public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ContextCompat.checkSelfPermission(
+                        MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestCameraPermission();
+                } else {
                     // Sends surface buffer to NDK
                     setSurface(surfaceHolder.getSurface());
                 }
+            }
 
-                @Override
-                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
 
-                }
+            }
 
-                @Override
-                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
 
-                }
-            });
-        }
+            }
+        });
     }
 
     private void requestCameraPermission() {
@@ -73,23 +67,13 @@ public class MainActivity extends Activity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == CAMERA_PERMISSION_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // [HACK] Restaring the app because camera preview is not displayed when granting camera
-                // premission for the first time
-                restartApp(mContext);
+                // Sends surface buffer to NDK
+                setSurface(mSurfaceHolder.getSurface());
             } else {
                 Toast.makeText(getApplicationContext(), "Please allow permission to use the camera",
                         Toast.LENGTH_SHORT).show();
             }
         }
-    }
-
-    private void restartApp (Context context) {
-        Intent mStartActivity = new Intent(context, MainActivity.class);
-        int mPendingIntentId = 123456;
-        PendingIntent mPendingIntent = PendingIntent.getActivity(context, mPendingIntentId,    mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
-        AlarmManager mgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 30, mPendingIntent);
-        System.exit(0);
     }
 
     @Override
